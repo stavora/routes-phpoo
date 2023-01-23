fffba21ac11d4ae6474517b06ddd0161534c4c98
Sylvio Tavora <sylvio@stavora.com.br>
2023-01-23 09:32:24 -0300

    Routes Commit

---
+++ app/routes/Routes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\routes;
+
+class Routes
+{
+    public static function get()
+    {
+        return [
+            'get' => [
+                '/' => 'HomeController@index',
+                '/user/[0-9]+' => 'UserController@edit',
+                '/product/[a-z]+/category/[a-z]+' => 'ProductController@show',
+                '/register' => 'RegisterController@store'
+            ],
+            'post' => [                
+                '/user/update/[0-9]+' => 'UserController@update',
+            ]
+        ];
+    }
+}
